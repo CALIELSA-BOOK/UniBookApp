01a03ab43c23f064decf8a9fc243dc8611d9bf60
Linnea <linneab01@hotmail.com>
2021-09-26 22:35:35 +0200

    Added ability to use photolibrary

--- UniBookApp/UniBookApp/ImageUploadView.swift
+++ UniBookApp/UniBookApp/ImageUploadView.swift
@@ -6,18 +6,44 @@
 //
 
 import SwiftUI
+import UIKit
 
 /** Add the camera functionality here **/
 
 struct ImageUploadView: View {
+    
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+    
+    @State private var activeImage = false
+    
     var body: some View {
-        ZStack{
-            Rectangle()
-                .fill(Color(red: 196/255, green: 196/255, blue: 196/255))
-                .frame(width: 150, height: 150)
-                .cornerRadius(10)
-                .padding(5)
-            Image(systemName: "plus.square")
+        VStack {
+            if activeImage == false {
+                ZStack{
+                    Rectangle()
+                        .fill(Color(red: 196/255, green: 196/255, blue: 196/255))
+                        .frame(width: 150, height: 150)
+                        .cornerRadius(7)
+                        .padding(5)
+                    Button(action: {
+                        self.isShowPhotoLibrary = true
+                        self.activeImage = true
+                    }) {
+                        Image(systemName: "plus.square")
+                    }
+                }
+            }else{
+                Image(uiImage: self.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150, height: 150)
+                    .clipped()
+                    .padding(5)
+            }
+        }
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
         }
     }
 }
